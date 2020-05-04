36799525a6ec3d5724c68da4959aa00eb914df2b
okdyy75 <okdyy75@gmail.com>
2020-05-04 19:05:09 +0900

    [fix] prod.envも追加

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,13 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FIREBASE_CONFIG : {
+    apiKey: '"<apiKey>"',
+    authDomain: '"<projectId>.firebaseapp.com"',
+    databaseURL: '"https://<projectId>.firebaseio.com"',
+    projectId: '"<projectId>"',
+    storageBucket: '"<projectId>.appspot.com"',
+    messagingSenderId: '"<messagingSenderId>"',
+    appId: "'<appId>'"
+  }
 }
