28b8e6bd1f6b31c541a0f102b325506e873d95dd
okdyy75 <okdyy75@gmail.com>
2020-09-24 18:32:54 +0900

    add example.env.js

---
+++ config/example.env.js
@@ -0,0 +1,13 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"local"',
+  FIREBASE_CONFIG : {
+    apiKey: '"<apiKey>"',
+    authDomain: '"<projectId>.firebaseapp.com"',
+    databaseURL: '"https://<projectId>.firebaseio.com"',
+    projectId: '"<projectId>"',
+    storageBucket: '"<projectId>.appspot.com"',
+    messagingSenderId: '"<messagingSenderId>"',
+    appId: "'<appId>'"
+  }
+}
